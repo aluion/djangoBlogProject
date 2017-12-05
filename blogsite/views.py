ddf84605330c96551ac6d8b675b1148ed5067ddd
Matt Zakrevsky <zakrevsm@oregonstate.edu>
2017-12-04 23:39:38 -0800

    working template from mysql database

--- blogsite/views.py
+++ blogsite/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.utils import timezone
+from .models import Post
+#  Create your views here.
 def post_list(request):
-    return render(request, 'blogsite/post_list.html' )
+    posts  = Post.objects.filter(created_on__lte=timezone.now()).order_by('created_on')
+    return render(request, 'blogsite/post_list.html', {'posts': posts} )
